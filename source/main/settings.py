13e46feb4358e1c3f3c7e5eadf126e34a4145564
Farid <thesomeq@gmail.com>
2020-07-30 19:32:40 +0600

    Добавлено приложение webapp

--- source/main/settings.py
+++ source/main/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for main project.
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webapp',
 ]
 
 MIDDLEWARE = [
